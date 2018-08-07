0a61264c7a0e0ae2568316e7db2d921f69848845
Grammer <675476193@qq.com>
2018-08-07 18:56:16 +0800

    Add:collision check

--- 01_ios/game_dazhuankuai/game_dazhuankuai/ViewController.m
+++ 01_ios/game_dazhuankuai/game_dazhuankuai/ViewController.m
@@ -7,21 +7,69 @@
 //
 
 #import "ViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface ViewController ()
 @property (strong, nonatomic) IBOutletCollection(UIImageView) NSArray *blocks;
+@property (weak, nonatomic) IBOutlet UIImageView *ball;
+@property (weak, nonatomic) IBOutlet UIImageView *paddle;
+
+@property (nonatomic,assign) CGPoint speed;
 
 @end
 
 @implementation ViewController
+
+- (void)onTimeout
+{
+    [self checkBlock];
+    [self checkScree];
+    [self updataBallLocation];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)checkScree
+{
+    if(CGRectGetMinY(self.ball.frame)<=0)
+    {
+//        self.speed.y = 5.0;
+    }
+    if(CGRectGetMinX(self.ball.frame)<=CGRectGetMinX(self.view.frame))
+    {
+        
+    }
+    if(CGRectGetMaxX(self.ball.frame)>=CGRectGetMaxX(self.view.frame))
+    {
+        
+    }
+    if(CGRectGetMaxY(self.ball.frame)>=CGRectGetMaxY(self.view.frame))
+    {
+//        lose
+//        self.speed.y = 5;
+    }
+    
+}
+
+- (void)checkBlock
+{
+    for (UIImageView *view in self.blocks) {
+//        if()
+    }
+    
+}
+
+- (void)updataBallLocation
+{
+    
+}
+
 - (IBAction)tapClick:(UITapGestureRecognizer *)sender {
     NSLog(@"%s",__func__);
+    //! start timer.
 }
 
 - (void)didReceiveMemoryWarning {
